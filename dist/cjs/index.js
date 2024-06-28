f9ae5f90820392de39868f8effd943e6f1e94691
Nirajan thapa <nnirajanthapaa@gmail.com>
2024-06-28 13:37:28 +0545

    hovering style is added to the buton

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -7,7 +7,7 @@ const BabalButton = ({ title, onclick, style, width, height, icon, hover, hoverS
     const defaultStyles = Object.assign({ backgroundColor: "#280154", display: 'flex', justifyContent: "center", alignItems: "center", gap: "8px", color: "white", border: "1px solid #280154", padding: "0.8rem", height, // Set the height from props
         width }, (hover && isHover ? hoverStyle : {}));
     const combinedStyle = Object.assign(Object.assign({}, defaultStyles), style);
-    return (React.createElement("button", { onClick: onclick, style: combinedStyle },
+    return (React.createElement("button", { onClick: onclick, onMouseEnter: () => { setIsHover(true); }, onMouseLeave: () => { setIsHover(false); }, style: combinedStyle },
         title,
         icon));
 };

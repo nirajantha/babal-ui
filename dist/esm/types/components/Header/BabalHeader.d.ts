a484426bb09f731bcb3e69f9b3556a65d9dcbd00
Nirajan thapa <nnirajanthapaa@gmail.com>
2024-07-01 08:12:46 +0545

    babal header added

---
+++ dist/esm/types/components/Header/BabalHeader.d.ts
@@ -0,0 +1,6 @@
+import React from 'react';
+export interface headerProps {
+    menus: string[];
+}
+declare const BabalHeader: ({ menus }: headerProps) => React.JSX.Element;
+export default BabalHeader;

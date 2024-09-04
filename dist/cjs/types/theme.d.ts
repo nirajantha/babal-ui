74041bbbb7fac99db2860aeee81aa1d4140693a9
Nirajan thapa <nnirajanthapaa@gmail.com>
2024-09-04 13:36:00 +0545

    code optimized

---
+++ dist/cjs/types/theme.d.ts
@@ -0,0 +1,6 @@
+import { DefaultTheme } from "styled-components";
+export interface CustomTheme extends DefaultTheme {
+    background?: string;
+    color?: string;
+    textColor?: string;
+}
---
+++ src/theme.ts
@@ -0,0 +1,7 @@
+import { DefaultTheme } from "styled-components";
+
+export interface CustomTheme extends DefaultTheme {
+  background?: string;
+  color?: string;
+  textColor?: string;
+}
